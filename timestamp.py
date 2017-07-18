221e27121bbd9a12487ca064a577dcf337f1adf0
samuel <songliantao@baidu.com>
2017-07-18 20:22:59 +0800

    time stamp done

--- timestamp.py
+++ timestamp.py
@@ -7,6 +7,7 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 SS_DEFAULT_SCALE = 30.0
+SS_DEFAULT_THICKNESS = 2
 
 class SSImage(object):
   def __init__(self, fname):
@@ -16,60 +17,66 @@ class SSImage(object):
 
   def size(self):
     shape = self.image.shape
-    print(shape)
+    #注意shape里面的值是高x宽xchannel
     return (shape[1], shape[0])
 
   def date(self):
     image = Image.open(self.fname)
     exif = image._getexif()
-    return (str)(exif[36867])
+    if exif and len(exif) > 36868:
+      return (str)(exif[36867])
+    else:
+      print(self.fname + " no timeStamp")
+      return None
 
   def rawImage(self):
     return self.image
     
-  def addTimestamp(timeStamp):
-    pass
-    
+  def addTimestamp(self, timeStamp):
+    text = self.date()
+    if text:
+      timeStamp.fitSize(self.size(), self.date())
+      timeStamp.drawOnImage(self.image)
+
+  def save(self):
+    cv2.imwrite(self.fname, self.image)
+
 class SSTimestamp(object):
   def __init__(self):
     self.font = cv2.FONT_HERSHEY_SIMPLEX
-    #字体scale可以设置为图片高度的1/10
-    self.fontScale = 1
     self.color = (0, 0, 0) #bgr
-    self.thickness = 3
 
-  def process(self, image):
-    imageWidth, imageHeight = image.size()
-    print(imageWidth)
-    print(imageHeight)
-    text = image.date()
-    textSize = cv2.getTextSize(text, self.font, self.fontScale, self.thickness)
-    height = textSize[0][1]
-    print(textSize)
-    print(height)
+  #size: (width, height)
+  def fitSize(self, size, text):
+    self.text = text
+
+    imageHeight = size[1]
+    imageWidth = size[0]
     desiredFontHeight = imageHeight / SS_DEFAULT_SCALE
-    self.fontScale = (int)(desiredFontHeight / height)
-    self.thickness = self.thickness * self.fontScale
+
+    fontScale = 1
+    textSize = cv2.getTextSize(text, self.font, fontScale, SS_DEFAULT_THICKNESS)
+    textHeight = textSize[0][1]
+
+    self.fontScale = (int)(desiredFontHeight / textHeight)
+    self.thickness = SS_DEFAULT_THICKNESS * self.fontScale
+
+    textSize = cv2.getTextSize(text, self.font, self.fontScale, self.thickness)
+    textWidth = textSize[0][0]
 
     #right margin = bottom margin = desiredFontHeight
-    textSize = cv2.getTextSize(text, self.font, self.fontScale, self.thickness)
-    width = textSize[0][0]
-    print(textSize)
-    print(width)
-    position = ((int)(imageWidth - width - desiredFontHeight), (int)(imageHeight - desiredFontHeight))
-    rawImage = image.rawImage()
-    print(position)
-    print(self.fontScale)
-    cv2.putText(rawImage, text, position, self.font, self.fontScale, self.color, self.thickness ,cv2.LINE_AA)
+    self.position = ((int)(imageWidth - textWidth - desiredFontHeight), (int)(imageHeight - desiredFontHeight))
 
-    return rawImage
+  def drawOnImage(self, image):
+    cv2.putText(image, self.text, self.position, self.font, self.fontScale, self.color, self.thickness ,cv2.LINE_AA)
 
 testImage = SSImage('1.jpg')
 timeStamp = SSTimestamp()
-result = timeStamp.process(testImage)
+testImage.addTimestamp(timeStamp)
+result = testImage.rawImage()
+testImage.save()
+
 showImage = result[:,:,::-1]
-
-
 
 plt.imshow(showImage)
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
